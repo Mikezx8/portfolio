21e2d294962172c6d9c1c9159d4522b62b3c17bb
Mikezx8 <mike777.zx@gmail.com>
2026-01-02 21:04:13 +0100

    Update py.py

--- py.py
+++ py.py
@@ -2,14 +2,15 @@ from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
-# Set the folder containing your HTML files
-HTML_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "html")
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Catch-all route
 @app.route('/', defaults={'path': 'index.html'})
 @app.route('/<path:path>')
 def serve_file(path):
-    # Serve any file in the HTML_DIR
-    return send_from_directory(HTML_DIR, path)
+    return send_from_directory(ROOT_DIR, path)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
